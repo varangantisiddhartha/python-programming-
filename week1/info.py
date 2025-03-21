bc472bc038c7b80be5a26478f1742b4850099c68
varangantisiddhartha <siddharthav2006@gmail.com>
2025-03-21 16:29:07 +0530

    Create info.py

---
+++ week1/info.py
@@ -0,0 +1,8 @@
+name=input("enter the name:")
+email=input("enter the email:")
+address=input("enter the address:")
+phn=int(input("enter the phnnum:"))
+print("name:",name)
+print("email",email)
+print("address",address)
+print("phnnum",phn)
